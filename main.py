c502149f28d15973c2459b1fad4abbfb61b5303c
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2017-05-06 17:41:57 -0300

    Abstraction about tags on response

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests as req
-
+import json
 problema = raw_input('Entedo com o nome do problema : \n')
 
 urlbase = 'https://api.stackexchange.com/2.2/search/excerpts?'
@@ -8,7 +8,9 @@ problema = problema.replace(' ', '%20')  #'Date%20formatting%20not%20working%20i
 url = urlbase+parametros+problema
 #print url
 r = req.get(url)
-conteudo = r.text
-print conteudo
+conteudo = str(r.text)
+conteudo = json.loads(conteudo)
+for i in range(0,len(conteudo['items'])):
+    print conteudo['items'][i]['tags']
 
 
