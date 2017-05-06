5494bb8bef1724695c73eed85406f1eb9e73bf3c
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2017-05-06 14:29:44 -0300

    Troca do urllib2 para o pack requests

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-
-import urllib2 as lib2
+import requests as req
 
 problema = raw_input('Entedo com o nome do problema : \n')
 
 urlbase = 'https://api.stackexchange.com/2.2/search/excerpts?'
 parametros = 'order=desc&sort=activity&accepted=True&closed=False&site=stackoverflow&q='
 problema = problema.replace(' ', '%20')  #'Date%20formatting%20not%20working%20in%20Swift%20'
-
-conteudo = lib2.urlopen(urlbase+parametros+problema).read()
+url = urlbase+parametros+problema
+#print url
+r = req.get(url)
+conteudo = r.text
 print conteudo
 
 
