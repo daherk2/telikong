ebc02a8837c7ee7f07f648cea6e199b602cda79a
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2017-05-07 09:53:55 -0300

    Insertion body information.

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ urlbase = 'https://api.stackexchange.com/2.2/search/excerpts?'
 parametros = 'order=desc&sort=activity&accepted=True&closed=False&site=stackoverflow&q='
 problema = problema.replace(' ', '%20')  #'Date%20formatting%20not%20working%20in%20Swift%20'
 url = urlbase+parametros+problema
-#print url
+print url
 r = req.get(url)
 conteudo = str(r.text)
 conteudo = json.loads(conteudo)
 for i in range(0,len(conteudo['items'])):
-    print conteudo['items'][i]['tags']
-
-
+    tags = conteudo['items'][i]['tags']
+    tagp = ''
+    for tag in tags:
+        tagp = tagp + " " + tag
+    print tagp
+    bodies = conteudo['items'][i]['body']
+    print bodies
