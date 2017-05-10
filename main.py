b209d01d80f70ba18c6e77fd9e3edab9def4a72c
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2017-05-10 10:13:18 -0300

    Parse as a method

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-import requests as req
-import json
-problema = raw_input('Entedo com o nome do problema : \n')
 
-urlbase = 'https://api.stackexchange.com/2.2/search/excerpts?'
-parametros = 'order=desc&sort=activity&accepted=True&closed=False&site=stackoverflow&q='
-problema = problema.replace(' ', '%20')  #'Date%20formatting%20not%20working%20in%20Swift%20'
-url = urlbase+parametros+problema
-print url
-r = req.get(url)
-conteudo = str(r.text)
-conteudo = json.loads(conteudo)
-for i in range(0,len(conteudo['items'])):
-    tags = conteudo['items'][i]['tags']
-    tagp = ''
-    for tag in tags:
-        tagp = tagp + " " + tag
-    print tagp
-    bodies = conteudo['items'][i]['body']
-    print bodies
-    bodies = conteudo['items'][i]['body']
-    print bodies
+
+
+def check_stackoverflow(problema):
+    import requests as req
+    import json
+    urlbase = 'https://api.stackexchange.com/2.2/search/excerpts?'
+    parametros = 'order=desc&sort=activity&accepted=True&closed=False&site=stackoverflow&q='
+    problema = problema.replace(' ', '%20')  #'Date%20formatting%20not%20working%20in%20Swift%20'
+    url = urlbase+parametros+problema
+    print url
+    r = req.get(url)
+    conteudo = str(r.text)
+    conteudo = json.loads(conteudo)
+    for i in range(0,len(conteudo['items'])):
+        if conteudo['items'][i]['is_accepted'] == 'true':
+            tags = conteudo['items'][i]['tags']
+            tagp = ''
+            for tag in tags:
+                tagp = tagp + " " + tag
+            print tagp
+            bodies = conteudo['items'][i]['body']
+            print bodies
+
+
+
+
