0d2a47db783e173117fd6cf8e77ad6e103c891a7
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2017-05-14 15:42:50 -0300

    Inserção de busca das respostas
    Inserção de buscas das respostas de cada pergunta que contenha parte da
    saída de exceção do código.

--- main.py
+++ main.py
@@ -6,22 +6,35 @@ def check_stackoverflow(problema):
     import json
     urlbase = 'https://api.stackexchange.com/2.2/search/excerpts?'
     parametros = 'order=desc&sort=activity&accepted=True&closed=False&site=stackoverflow&q='
-    problema = problema.replace(' ', '%20')  #'Date%20formatting%20not%20working%20in%20Swift%20'
+    problema = str(problema).replace(' ', '%20')  #'Date%20formatting%20not%20working%20in%20Swift%20'
     url = urlbase+parametros+problema
     print url
     r = req.get(url)
-    conteudo = str(r.text)
+    conteudo = u' '.join((r.text, '')).encode('utf-8').strip()
     conteudo = json.loads(conteudo)
     for i in range(0,len(conteudo['items'])):
-        if conteudo['items'][i]['is_accepted'] == 'true':
-            tags = conteudo['items'][i]['tags']
-            tagp = ''
-            for tag in tags:
-                tagp = tagp + " " + tag
-            print tagp
-            bodies = conteudo['items'][i]['body']
-            print bodies
+        tags = conteudo['items'][i]['tags']
+        tagp = ''
+        for tag in tags:
+            tagp = tagp + " " + tag
+        print tagp
+        bodies = conteudo['items'][i]['body']
+        print bodies
+        idq = conteudo['items'][i]['question_id']
+        base_answers = 'https://api.stackexchange.com/2.2/questions/'+str(idq)+'/answers?order=desc&sort=activity&site=stackoverflow'
+        r = req.get(base_answers)
+        cont_answer = u' '.join((r.text, '')).encode('utf-8').strip()
+        data_answer = json.loads(cont_answer)
+        for i in range(0,len(data_answer['items'])):
+            print data_answer['items'][i]
 
-
-
+###################### Exemplo ########################
+try:
+    a = 3/0
+    print a
+except Exception as e:
+    try:
+        check_stackoverflow(e)
+    except Exception as e:
+        check_stackoverflow(e)
 
