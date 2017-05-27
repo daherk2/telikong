32031844a15a393e1c767701dead70dbacfc8e1c
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2017-05-27 13:53:05 -0300

    Inserção do rethinkdb no projeto para salvar os dados

--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 from flask import Flask
 import app
 import re
+import rethinkdb as r
+import json
 
 apps = Flask(__name__)
 
 @apps.route('/exception/<excep>')
 def main(excep):
-    return app.check_stackoverflow(excep)
+    model = '''
+    {
+    	"links": xxxxx
+    }
+    '''
+    var = app.check_stackoverflow(excep)
+    conn = r.connect('localhost', 32901).repl()
+    links = var.split(',')
+    texto = ''
+    model = str(model).replace('xxxxx', str(links[1:]))
+    model = model.replace("'", '"')
+    #print model
+    r.db('Telikong').table('Exceptions').insert(json.loads(model)).run()
+    return var
 
 
 if __name__ == '__main__':
